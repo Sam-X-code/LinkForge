3d17d7630cdb4097987576fcb5ea475ad1c37eed
Shivam Sachan <sachanshivam099@gmail.com>
2026-06-26 20:52:55 +0530

    redis connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,28 @@
 import dotenv from "dotenv"
 import app from "./app.js"
 import connectDB from "./db/dbConnection.js"
+import redisClient from "./config/redis.js";
 
 dotenv.config({
     path:"./.env"
 })
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000 , ()=>{
-        console.log(`Server is running at port : ${process.env.PORT}`
-        ); 
-    })
+.then(async () => {
+    try {
+        await redisClient.connect();
+        console.log("✅ Redis Connected");
+    } catch (error) {
+        console.log("⚠️ Redis unavailable. Running without cache.");
+    }
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+
 })
 .catch((error) => {
-    console.log("MONGODB connection failed!!",error);
-})
+    console.log("Server startup failed!!", error);
+});
 
 
